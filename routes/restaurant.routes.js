5b5eeecdbbfeccac130154fe91412416bf514fde
Bhalani-Mauli <maulibhalani13@gmail.com>
2023-11-20 12:44:19 +0100

    edit restaurants api with database

--- routes/restaurant.routes.js
+++ routes/restaurant.routes.js
@@ -1,14 +1,22 @@
 const router = require("express").Router();
 const Restaurant = require("../models/Restaurant.model");
 
-router.get("/", (req, res) =>
+router.get("/create", (req, res) =>
   res.render("../views/restaurants/restaurant-create.hbs")
 );
-router.get("/", (req, res) => res.render("../views/partials/rating.hbs"));
 
-router.post("/", (req, res, next) => {
-  const { name, email, street, pincode, city, typeOfCuisine, rating, image } =
-    req.body;
+router.post("/create", (req, res, next) => {
+  const {
+    name,
+    email,
+    street,
+    pincode,
+    city,
+    typeOfCuisine,
+    rating,
+    image,
+    restaurantOnBoarded,
+  } = req.body;
   Restaurant.create({
     name,
     email,
@@ -18,6 +26,7 @@ router.post("/", (req, res, next) => {
     typeOfCuisine,
     rating,
     image,
+    restaurantOnBoarded,
   })
     .then((dbRestaurant) => {
       //TODO: redirect to proper page
@@ -28,4 +37,58 @@ router.post("/", (req, res, next) => {
     .catch((error) => next(error));
 });
 
+router.get("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  Restaurant.findById(id)
+    .then((dbData) => {
+      res.render("../views/restaurants/restaurant-edit.hbs", {
+        id,
+        name: dbData.name,
+        email: dbData.email,
+        street: dbData.street,
+        city: dbData.city,
+        pincode: dbData.pincode,
+        rating: dbData.rating,
+        typeOfCuisine: dbData.typeOfCuisine,
+        restaurantOnBoarded: dbData.restaurantOnBoarded,
+      });
+    })
+    .catch((error) => console.log(error));
+});
+
+router.post("/edit/:id", (req, res, next) => {
+  const { id } = req.params;
+
+  const {
+    name,
+    email,
+    street,
+    pincode,
+    city,
+    typeOfCuisine,
+    rating,
+    image,
+    restaurantOnBoarded,
+  } = req.body;
+
+  Restaurant.findByIdAndUpdate(
+    id,
+    {
+      name,
+      email,
+      street,
+      pincode,
+      city,
+      typeOfCuisine,
+      rating,
+      image,
+      restaurantOnBoarded,
+    },
+    { new: true }
+  )
+    .then((dbRestaurant) => {
+      res.send("<h2>Data Updated successfully.");
+    })
+    .catch((error) => next(error));
+});
 module.exports = router;
