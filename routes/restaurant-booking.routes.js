a3d65f7ab9c39581ff49f665ee02f7c3107e0080
Bhalani-Mauli <maulibhalani13@gmail.com>
2023-11-23 00:27:51 +0100

    add user data to database when creating a booking

--- routes/restaurant-booking.routes.js
+++ routes/restaurant-booking.routes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const Restaurant = require("../models/Restaurant.model");
 const Booking = require("../models/Booking.model");
+const { isLoggedIn } = require("../middleware/middleware.js");
 
-router.get("/:id", (req, res) => {
+router.get("/:id", isLoggedIn, (req, res) => {
   const id = req.params.id;
 
   Restaurant.findById(id)
@@ -15,15 +16,16 @@ router.get("/:id", (req, res) => {
     .catch((error) => console.log(error));
 });
 
-router.post("/", (req, res) => {
+router.post("/", isLoggedIn, (req, res) => {
   const { numOfPeople, date, time } = req.body;
+  const userEmail = req.session.currentUser.email;
 
   const newBooking = new Booking({
     numOfPeople,
     date,
     time,
+    userEmail,
   });
-
   newBooking
     .save()
     .then(() => {
